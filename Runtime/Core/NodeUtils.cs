b754d3e95c4ea22846f35c997aa21289df03927a
ACELANDWORKSHOP\parsu <parsue@gmail.com>
2024-11-02 14:32:49 +0800

    handle Settings in new way

---
+++ Runtime/Core/NodeUtils.cs
@@ -0,0 +1,19 @@
+﻿using AceLand.NodeFramework.ProjectSetting;
+using UnityEngine;
+
+namespace AceLand.NodeFramework.Core
+{
+    internal static class NodeUtils
+    {
+        public static NodeFrameworkSettings Settings
+        {
+            get
+            {
+                _settings ??= Resources.Load<NodeFrameworkSettings>(nameof(NodeFrameworkSettings));
+                return _settings;
+            }
+        }
+        
+        private static NodeFrameworkSettings _settings;
+    }
+}
--- Runtime/Node_Getter.cs
+++ Runtime/Node_Getter.cs
@@ -2,24 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using AceLand.NodeFramework.Core;
-using AceLand.NodeFramework.ProjectSetting;
 using AceLand.TaskUtils;
-using AceLand.TaskUtils.PromiseAwaiter;
 using UnityEngine;
 
 namespace AceLand.NodeFramework
 {
-    public static class Node
-    {
-        public static NodeFrameworkSettings Settings
-        {
-            get => _settings ?? Resources.Load<NodeFrameworkSettings>(nameof(NodeFrameworkSettings));
-            internal set => _settings = value;
-        }
-        
-        private static NodeFrameworkSettings _settings;
-    }
-    
     public partial class Node<T>
     {
         public static Promise<Node<T>> Get() =>
@@ -33,8 +20,8 @@ namespace AceLand.NodeFramework
 
         private static async Task<Node<T>> GetNode()
         {
-            var aliveToken = TaskHelper.ApplicationAliveToken;
-            var targetTime = Time.realtimeSinceStartup + Node.Settings.NodeGetterTimeout;
+            var aliveToken = Promise.ApplicationAliveToken;
+            var targetTime = Time.realtimeSinceStartup + NodeUtils.Settings.NodeGetterTimeout;
     
             while (!aliveToken.IsCancellationRequested && Time.realtimeSinceStartup < targetTime)
             {
@@ -50,7 +37,7 @@ namespace AceLand.NodeFramework
 
         private static async Task<Node<T>> GetNode(string id)
         {
-            var aliveToken = TaskHelper.ApplicationAliveToken;
+            var aliveToken = Promise.ApplicationAliveToken;
             var targetTime = Time.realtimeSinceStartup + 1.5f;
     
             while (!aliveToken.IsCancellationRequested && Time.realtimeSinceStartup < targetTime)
